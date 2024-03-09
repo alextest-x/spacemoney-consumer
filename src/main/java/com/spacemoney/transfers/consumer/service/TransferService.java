cc47c806e41b9c1b7fcd0b1ebcf892dfcc5d2cb2
alextest-x <alexsnet01@gmail.com>
2024-03-09 13:17:55 -0600

    consumer sprigboot

---
+++ src/main/java/com/spacemoney/transfers/consumer/service/TransferService.java
@@ -0,0 +1,9 @@
+package com.spacemoney.transfers.consumer.service;
+
+import com.spacemoney.transfers.consumer.domain.Transfer;
+
+public interface TransferService {
+
+    public void registerTransfer(Transfer transfer);
+
+}
